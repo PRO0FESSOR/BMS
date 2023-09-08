415e4b932cfab9f1c27a7075652ecafe32bb98e7
PRO0FESSOR <siddharth96549@gmail.com>
2023-09-08 16:55:57 +0530

    last call

---
+++ core/query.py
@@ -0,0 +1,234 @@
+from django.db import connection
+
+# select current date
+
+def getSelectedDate():
+    query1 = "SELECT currentDate as DATE FROM tblselecteddate;"
+
+    with connection.cursor() as cursor:
+        cursor.execute(query1)
+        single_value = cursor.fetchone()
+        data1= single_value[0] if single_value else None
+    
+    return data1
+
+# get date time 
+
+def getFromToDT():
+    query2 = "SELECT fromDT as fDT, toDT as tDT FROM tbldtspan;"
+
+    with connection.cursor() as cursor:
+        cursor.execute(query2)
+        single_value = cursor.fetchone()
+        return single_value
+
+# update date 
+
+def updateSelectedDate(from_date,to_date):
+    query4 = f"UPDATE tblselecteddate SET currentDate = '{from_date}';"
+    existing_from_dt, existing_to_dt = getFromToDT()
+    if existing_from_dt and existing_to_dt:
+        # Split the existing time from the output ('2023-07-13 00:00:01', '2023-07-13 23:59:59')
+        existing_from_time = existing_from_dt.split()[1]
+        existing_to_time = existing_to_dt.split()[1]
+
+        # Combine the updated date with the existing time
+        from_dt_tobeupdated = f"{from_date} {existing_from_time}"
+        to_dt_tobeupdated = f"{to_date} {existing_to_time}"
+
+        # Update the data in the database
+        query5 = f"UPDATE tbldtspan SET fromDT = '{from_dt_tobeupdated}';"
+        query6 = f"UPDATE tbldtspan SET toDT = '{to_dt_tobeupdated}';"
+
+        with connection.cursor() as cursor:
+            cursor.execute(query5)
+            cursor.execute(query6)    
+
+    with connection.cursor() as cursor:
+        cursor.execute(query4)
+
+# get to date 
+
+def gettodate():
+    existing_from_dt, existing_to_dt = getFromToDT()
+    existing_to_date = existing_to_dt.split()[0]
+    return existing_to_date
+
+# switch status 
+
+def getSwitchStatus():
+    query3 = "SELECT STATUS FROM tblhrsswitch;"
+    with connection.cursor() as cursor:
+        cursor.execute(query3)
+        single_value = cursor.fetchone()
+        data1= single_value[0] if single_value else None
+        return data1
+
+# change status
+
+def toggleSwitchStatus(status):
+    query7 = f"UPDATE tblhrsswitch SET STATUS = '{status}';"
+    if status == "OFF":
+        NEW_FROM_TIME = "00:00:01"
+        NEW_TO_TIME = "23:59:59"
+    else:        
+        NEW_FROM_TIME = "09:30:00"
+        NEW_TO_TIME = "19:00:00"
+    
+    existing_from_dt, existing_to_dt = getFromToDT()
+    if existing_from_dt and existing_to_dt:
+        # Split the existing time from the output ('2023-07-13 00:00:01', '2023-07-13 23:59:59')
+        existing_from_date = existing_from_dt.split()[0]
+        existing_to_date = existing_to_dt.split()[0]
+
+        # Combine the updated date with the existing time
+        from_tm_tobeupdated = f"{existing_from_date} {NEW_FROM_TIME}"
+        to_tm_tobeupdated = f"{existing_to_date} {NEW_TO_TIME}"
+
+        # Update the data in the database
+        query8 = f"UPDATE tbldtspan SET fromDT = '{from_tm_tobeupdated}';"
+        query9 = f"UPDATE tbldtspan SET toDT = '{to_tm_tobeupdated}';"
+
+        with connection.cursor() as cursor:
+            cursor.execute(query8)
+            cursor.execute(query9)  
+    
+    with connection.cursor() as cursor:
+        cursor.execute(query7)
+
+# get user data 
+
+def glanceShortcut():
+    existing_from_dt, existing_to_dt = getFromToDT()
+    existing_from_date = existing_from_dt.split()[0]
+    existing_to_date = existing_to_dt.split()[0]
+    print(existing_from_date,existing_to_date)
+    time = getSwitchStatus()
+    if time == "on":
+        query13 = f"""       
+        SELECT USERID,
+        TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(W_PROTIME))), '%H:%i:%s') AS total_protime,
+        TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(W_UNPROTIME))), '%H:%i:%s') AS total_unprotime,
+        TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(W_IDLETIME))), '%H:%i:%s') AS total_idletime,
+        MIN(W_INTIME) AS min_intime,
+        MAX(W_OUTTIME) AS max_outtime,
+        SUM(W_PROCOUNT) AS total_procount,
+        SUM(W_UNPROCOUNT) AS total_unprocount,
+        SUM(W_IDLECOUNT) AS total_idlecount
+        FROM newproworkingdetails_page1
+        WHERE DATE BETWEEN '{existing_from_date}' AND '{existing_to_date}'
+        GROUP BY USERID;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query13)
+            results = cursor.fetchall()
+
+    else:
+        query13 = f"""
+        SELECT USERID,
+        TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(PROTIME))), '%H:%i:%s') AS total_protime,
+        TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(UNPROTIME))), '%H:%i:%s') AS total_unprotime,
+        TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(IDLETIME))), '%H:%i:%s') AS total_idletime,
+        MIN(INTIME) AS min_intime,
+        MAX(OUTTIME) AS max_outtime,
+        SUM(PROCOUNT) AS total_procount,
+        SUM(UNPROCOUNT) AS total_unprocount,
+        SUM(IDLECOUNT) AS total_idlecount
+        FROM newproworkingdetails_page1
+        WHERE DATE BETWEEN '{existing_from_date}' AND '{existing_to_date}'
+        GROUP BY USERID;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query13)
+            results = cursor.fetchall()
+            
+    return results
+
+# get single user data 
+
+def onlyglanceShortcut(user):
+    existing_from_dt, existing_to_dt = getFromToDT()
+    existing_from_date = existing_from_dt.split()[0]
+    existing_to_date = existing_to_dt.split()[0]
+    time = getSwitchStatus()
+    if time == "on":
+        query13 = f"""
+        SELECT USERID,
+            TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(W_PROTIME))), '%H:%i:%s') AS total_protime,
+            TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(W_UNPROTIME))), '%H:%i:%s') AS total_unprotime,
+            TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(W_IDLETIME))), '%H:%i:%s') AS total_idletime,
+            MIN(W_INTIME) AS min_intime,
+            MAX(W_OUTTIME) AS max_outtime,
+            SUM(W_PROCOUNT) AS total_procount,
+            SUM(W_UNPROCOUNT) AS total_unprocount,
+            SUM(W_IDLECOUNT) AS total_idlecount,
+            (SELECT IP FROM newproworkingdetails_page1 WHERE USERID = '{user}' AND DATE = '{existing_from_date}') AS ip,
+            (SELECT IP_LOCATION FROM newproworkingdetails_page1 WHERE USERID = '{user}' AND DATE = '{existing_from_date}') AS ip_location
+        FROM newproworkingdetails_page1
+        WHERE USERID = '{user}' AND DATE BETWEEN '{existing_from_date}' AND '{existing_to_date}'
+        GROUP BY USERID;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query13)
+            results = cursor.fetchall()
+    else:
+        query13 = f"""
+        SELECT USERID,
+            TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(PROTIME))), '%H:%i:%s') AS total_protime,
+            TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(UNPROTIME))), '%H:%i:%s') AS total_unprotime,
+            TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(IDLETIME))), '%H:%i:%s') AS total_idletime,
+            MIN(INTIME) AS min_intime,
+            MAX(OUTTIME) AS max_outtime,
+            SUM(PROCOUNT) AS total_procount,
+            SUM(UNPROCOUNT) AS total_unprocount,
+            SUM(IDLECOUNT) AS total_idlecount,
+            (SELECT IP FROM newproworkingdetails_page1 WHERE USERID = '{user}' AND DATE = '{existing_from_date}') AS ip,
+            (SELECT IP_LOCATION FROM newproworkingdetails_page1 WHERE USERID = '{user}' AND DATE = '{existing_from_date}') AS ip_location
+        FROM newproworkingdetails_page1
+        WHERE USERID = '{user}' AND DATE BETWEEN '{existing_from_date}' AND '{existing_to_date}'
+        GROUP BY USERID;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query13)
+            results = cursor.fetchall()
+            
+
+    return results
+
+
+#page2
+
+def alltables(user):
+    existing_from_dt, existing_to_dt = getFromToDT()
+    existing_from_date = existing_from_dt.split()[0]
+    existing_to_date = existing_to_dt.split()[0]
+    time = getSwitchStatus()
+
+    if time == "on":
+        query14 = f"""           
+            SELECT TITLE, PROCESSNAME, PRO_TYPE, W_TIME_FOR_DAY, TASK_TYPE
+            FROM newproworkingdetails_page2
+            WHERE USERID = '{user}' AND DATE BETWEEN '{existing_from_date}' AND '{existing_to_date}'
+            ORDER BY W_TIME_FOR_DAY DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query14)
+            data = cursor.fetchall()
+    else:
+        query14 = f"""
+            SELECT TITLE, PROCESSNAME, PRO_TYPE, TIME_FOR_DAY, TASK_TYPE
+            FROM newproworkingdetails_page2
+            WHERE USERID = '{user}' AND DATE BETWEEN '{existing_from_date}' AND '{existing_to_date}'
+            ORDER BY TIME_FOR_DAY DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query14)
+            data = cursor.fetchall()
+
+
+    return data 
+
+
+
+
+
---
+++ core/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import render , redirect
+from django.http import HttpResponseRedirect , HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
+from .forms import  LoginForm
+from django.db import connection
+from .query import getSelectedDate , updateSelectedDate , getFromToDT , getSwitchStatus , toggleSwitchStatus,glanceShortcut ,alltables,onlyglanceShortcut ,gettodate
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+
+
+# Create your views here.
+
+#loginform
+
+def user_login(request):
+    if request.method=="POST":
+
+        form = LoginForm(request=request , data=request.POST)
+        if form.is_valid():
+            uname = form.cleaned_data['username']
+            upass = form.cleaned_data['password']
+            user = authenticate(username=uname,password=upass)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect('/') 
+    else:
+        form = LoginForm()
+
+    return render(request,'core/login.html',{"form":form})
+
+
+#logout
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+
+#description 
+
+def description(request,user):
+
+
+    toggle_value = ""
+    From_datee=getSelectedDate()
+    emp_value=None
+    if request.method == 'POST':
+
+            print(request.get_full_path())
+            From_datee=request.POST.get('currdate')
+            To_datee=request.POST.get('currdating')
+            toggle_value = request.POST.get('toggler')
+            emp_value = request.POST.get('employee')
+
+            #handeling toggle
+
+            if toggle_value==None:
+                toggle_value="OFF"
+
+            
+            toggleSwitchStatus(toggle_value) 
+
+            #handeling datetime
+
+            if From_datee:
+                if To_datee:
+                    updateSelectedDate(From_datee,To_datee)
+                else:
+                    updateSelectedDate(From_datee,From_datee)
+            
+            #handeling search 
+
+            if emp_value!="Open this select menu":
+                return HttpResponseRedirect(f'/description/{emp_value}/') 
+
+
+
+
+    user_data = onlyglanceShortcut(user)
+
+    data = alltables(user)
+
+    rdp_data = []
+    browsing_data = []
+    process_data = []
+
+    for entry in data:
+        if entry[2] == 'RDP':
+            rdp_data.append(entry)
+            process_data.append(entry)
+        elif entry[2] == 'BROWSING':
+            browsing_data.append(entry)
+            process_data.append(entry)
+        else:
+            process_data.append(entry)
+                
+    
+    context = {
+        'user':user,
+        'user_data': user_data,
+        'rdpdata':rdp_data,
+        'brdata':browsing_data,
+        'prdata':process_data,
+        "data3":getSwitchStatus(),
+        "fromdate":getSelectedDate(),
+        "todate":gettodate()
+    }
+    return render(request,'core/description.html',context)
+
+
+
+#home 
+  
+def home(request):
+    if request.user.is_authenticated : 
+
+        toggle_value = ""
+        From_datee=getSelectedDate()
+        emp_value=None
+        if request.method == 'POST':
+
+            print(request.get_full_path())
+            From_datee=request.POST.get('currdate')
+            To_datee=request.POST.get('currdating')
+            toggle_value = request.POST.get('toggler')
+            emp_value = request.POST.get('employee')
+
+            #handeling toggle
+
+            if toggle_value==None:
+                toggle_value="OFF"
+            
+            toggleSwitchStatus(toggle_value) 
+
+            #handeling datetime
+
+            if From_datee:
+                if To_datee:
+                    updateSelectedDate(From_datee,To_datee)
+                else:
+                    updateSelectedDate(From_datee,From_datee)                
+
+            
+            #handeling search 
+
+            if emp_value!="Open this select menu":
+                return HttpResponseRedirect(f'/description/{emp_value}/') 
+                 
+   
+        return render(request,'core/home.html',{"data2":getFromToDT(),"fromdate":getSelectedDate(),"data3":getSwitchStatus(),"data5":glanceShortcut(),"data6":emp_value,"todate":gettodate()})
+        
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+
